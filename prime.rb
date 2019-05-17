15b2fac86039872054a44339d6f7a32ad4ccb8d6
Alexander Riccio <test35965@gmail.com>
2019-05-17 19:26:20 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,1 +1,11 @@
 # Add  code here!
+def prime?(num)
+  for i in (1..num) do
+    for j in (1..(num/2)) do
+      if (i * j) == num
+        return true
+      end
+    end
+  end
+  return false
+end
